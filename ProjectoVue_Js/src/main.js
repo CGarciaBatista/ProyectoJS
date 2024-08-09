38b8fd8d193796e5e9a338de2e09215ecfd28a36
Roger Oliva <rogeroliva44@gmail.com>
2024-08-08 21:19:29 -0400

    se acabo
    ProjectoJS hecho con el framework VUE

---
+++ ProjectoVue_Js/src/main.js
@@ -0,0 +1,10 @@
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import BaseCard from './components/Ui/BaseCard.vue'
+import CardButton from '../src/components/Ui/CardButton.vue'
+const app = createApp(App)
+
+app.component('base-card', BaseCard)
+app.component('card-button', CardButton)
+app.mount("#app")
