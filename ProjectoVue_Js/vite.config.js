6102b9ba3eddd21400eb74035f22402c0e22377e
CGarciaBatista <cgarcia@ceis.cujae.edu.cu>
2024-08-10 02:05:11 -0400

    Update vite.config.js

--- ProjectoVue_Js/vite.config.js
+++ ProjectoVue_Js/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ProyectoJS/",
+  base: "/ProyectoJS/ProjectoVue_Js/",
   plugins: [
     vue(),
   ],
