1391940658394600a278910f2f452cd3abdba131
Carlos Garcia <cgarcia@ceis.cujae.edu.cu>
2024-08-10 01:59:29 -0400

    dist

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import BaseCard from './components/Ui/BaseCard.vue'
-import CardButton from '../src/components/Ui/CardButton.vue'
-const app = createApp(App)
-
-app.component('base-card', BaseCard)
-app.component('card-button', CardButton)
-app.mount('#app')
